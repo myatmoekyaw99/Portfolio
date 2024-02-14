633712e3d8ae13a46aee194b33dd7decdb8d42c7
mmk <myat1999kyaw@gmail.com>
2024-02-14 23:26:32 +0630

    add profile page

--- index.js
+++ index.js
@@ -12,25 +12,6 @@ toggleBtn.onclick = function(){
 };
 
 
-/********AUTOMATIC SLIDER***********/
-
-// let firstIndex = 0;
-// function automaticSlide(){
-//     setTimeout(automaticSlide,2000);
-//     let pics;
-//     const img = document.querySelectorAll('img');
-//     for(pics = 0; pics < img.length ; pics++){
-//         img[pics].style.display = 'none';
-//     }
-//     firstIndex++;
-//     if(firstIndex > img.length){
-//         firstIndex = 1;
-//     }
-//     img[firstIndex-1].style.display = 'block';
-// }
-
-// automaticSlide();
-
 /********************Owl slider***********************/
 $('.owl-carousel').owlCarousel({
   loop:true,
